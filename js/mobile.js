2d53018e171363c46a98d725adc8d173263bb9a0
minscHelena <helena.schiavon@escola.pr.gov.br>
2024-11-18 22:34:39 -0300

    feito o basico do css

---
+++ js/mobile.js
@@ -0,0 +1,5 @@
+/*pesquisar como fazer a pesquisa e filtro, puxar por um nome ou letra
+fazer adiçã das traferas já formatadas no html na tag <div class="container-task-list">
+fazer o alterar, excluir e concluir
+no concluir, a tarefa tem de sumir
+criar o alert modal antes de cada alteração*/
