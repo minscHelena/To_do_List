211b48ad7ec361627889183c91b35e53245ac04f
minscHelena <helena.schiavon@escola.pr.go.br>
2024-11-24 20:34:09 -0300

    tentei mechei no js mas não deu

--- js/mobile.js
+++ js/mobile.js
@@ -3,3 +3,16 @@ fazer adiçã das traferas já formatadas no html na tag <div class="container-t
 fazer o alterar, excluir e concluir
 no concluir, a tarefa tem de sumir
 criar o alert modal antes de cada alteração*/
+
+
+const openModal = document.querySelector("button");
+const modal = document.querySelector("dialog");
+const closeModal = document.querySelector("dialog button");
+
+openModal.onclick = function(){
+    modal.showModal();
+
+}
+closeModal.onclick = function (){
+    modal.close();
+}
